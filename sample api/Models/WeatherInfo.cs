65ba97ed53af31d36e132d21927aeb754b06467c
Koshi30 <kousicksrinivas@gmail.com>
2025-11-18 18:44:26 +0530

    first

---
+++ sample api/Models/WeatherInfo.cs
@@ -0,0 +1,9 @@
+namespace SampleAPI.Models
+{
+    public class WeatherInfo
+    {
+        public int Id { get; set; }
+        public string? City { get; set; }
+        public int Temperature { get; set; }
+    }
+}
